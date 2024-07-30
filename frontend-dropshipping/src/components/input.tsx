43e987d69d59e41c4716371d1e3563ecd5fd773b
ArthurRodrigomarques <ArthurRodrigomarques@users.noreply.github.com>
2024-07-29 22:45:23 -0300

    Search
    Search

--- frontend-dropshipping/src/components/input.tsx
+++ frontend-dropshipping/src/components/input.tsx
@@ -1,42 +1,23 @@
 "use client";
 
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { Button } from './ui/button';
 import { Input } from './ui/input';
-import { fetchProducts } from '@/services/search';
-
-interface Product {
-    id: string;
-    name: string;
-    // Adicione outros campos necessários aqui, se houver
-}
 
 export default function InputNavigation() {
     const [searchTerm, setSearchTerm] = useState<string>('');
-    const [searchResults, setSearchResults] = useState<Product[]>([]);
+    const router = useRouter();
 
     const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearchTerm(e.target.value);
     };
 
-    const handleSearchSubmit = async (e: React.FormEvent) => {
+    const handleSearchSubmit = (e: React.FormEvent) => {
         e.preventDefault();
         if (!searchTerm) return;
 
-        try {
-            const data = await fetchProducts(searchTerm);
-
-            const normalizedSearchTerm = searchTerm.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
-            const filteredData = Array.isArray(data)
-                ? data.filter(product => 
-                    product.name.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase().includes(normalizedSearchTerm)
-                )
-                : [data];
-
-            setSearchResults(filteredData);
-        } catch (error) {
-            console.error('Erro ao buscar produtos:', error);
-        }
+        router.push(`/search?query=${encodeURIComponent(searchTerm)}`);
     };
 
     return (
@@ -52,17 +33,6 @@ export default function InputNavigation() {
                     />
                     <Button type="submit">Pesquisar</Button>
                 </form>
-            </div>
-            <div>
-                {searchResults.length > 0 ? (
-                    <ul>
-                        {searchResults.map((result: Product) => (
-                            <li key={result.id}>{result.name}</li>
-                        ))}
-                    </ul>
-                ) : (
-                    <p>Nenhum resultado encontrado</p>
-                )}
             </div>
         </div>
     );
